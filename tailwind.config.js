e30c60fd6885b877c5052d4e8d325baafb2b87b7
srihari <srihariamit@gmail.com>
2025-03-29 17:50:03 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Add your project's file extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
